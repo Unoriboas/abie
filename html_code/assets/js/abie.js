b2ca632ddce72d6c14a26d67aa21682b9459d698
sCrONcHerz <ricknmortyfan949@gmail.com>
2022-03-23 22:42:52 +0100

    aaa again!

--- html_code/assets/js/abie.js
+++ html_code/assets/js/abie.js
@@ -12,25 +12,3 @@
 * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
 * Imported to make sure everything works!
 */
-
-
-// smooth scroll
-$(document).ready(function(){
-	$(".nav-link").on('click', function(event) {
-
-    	if (this.hash !== "") {
-
-			event.preventDefault();
-
-			var hash = this.hash;
-
-			$('html, body').animate({
-				scrollTop: $(hash).offset().top
-			}, 700, function(){
-				window.location.hash = hash;
-			});
-      	} 
-    });
-});
-
-
